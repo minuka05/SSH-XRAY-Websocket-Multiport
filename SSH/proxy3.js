865009cfed64152aa6a3568f14aeb97ee3c6cfeb
Minuka <124237692+minuka05@users.noreply.github.com>
2024-05-25 19:55:19 +0530

    small fixes

--- SSH/proxy3.js
+++ SSH/proxy3.js
@@ -56,7 +56,7 @@ const server = net.createServer();
 server.on('connection', function(socket) {
     var packetCount = 0;
     //var handshakeMade = false;
-    socket.write("HTTP/1.1 101 Autoscript By bokirrrrr\r\nContent-Length: 1048576000000\r\n\r\n", function(err) {
+    socket.write("HTTP/1.1 101 Script By bokirrrrr\r\nContent-Length: 1048576000000\r\n\r\n", function(err) {
         if(err) {
             console.log("[SWRITE] Failed to write response to " + socket.remoteAddress + ":" + socket.remotePort + ", error: " + err);
         }
